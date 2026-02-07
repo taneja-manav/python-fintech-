17f4549229f27bb2a25110f8a35e2cab15765bf3
taneja-manav <87687683+taneja-manav@users.noreply.github.com>
2026-02-07 22:59:55 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+# numpy is a library for numerical operations
+# pandas is a library for data manipulation and analysis
+# matplotlib is a library for data visualization
+# seaborn is a library for  advanced data visualization
+# scikit-learn is a library for machine learning
+# beautifulsoup is a library for web scrapping
+# selenium is a library for web automation
+# scikit-learn is a library for machine learning
+
+
+
+a = 10
+b = 20
+print("a + b")
